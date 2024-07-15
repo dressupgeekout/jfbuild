f956d1efef1500af12cbb3146cab6904f5b57383
Charlotte Koch <dressupgeekout@gmail.com>
2024-07-15 16:00:58 -0700

    The byte-swap functions on BSD don't have the leading underscores

--- include/compat.h
+++ include/compat.h
@@ -120,9 +120,9 @@ typedef unsigned __int64 uint64_t;
 #  define B_LITTLE_ENDIAN 0
 #  define B_BIG_ENDIAN    1
 # endif
-# define B_SWAP64(x) __bswap64(x)
-# define B_SWAP32(x) __bswap32(x)
-# define B_SWAP16(x) __bswap16(x)
+# define B_SWAP64(x) bswap64(x)
+# define B_SWAP32(x) bswap32(x)
+# define B_SWAP16(x) bswap16(x)
 
 #elif defined(__APPLE__)
 # if defined(__LITTLE_ENDIAN__)
